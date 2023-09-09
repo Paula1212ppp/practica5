10c0ba35be06c5b6360cba3c255af5ddcd734353
“lisaleman“ <lisaleman@gmail.com>
2023-09-09 09:25:47 -0600

    haciendo cambios de apariencia

--- index.php
+++ index.php
@@ -7,19 +7,20 @@
 <body>
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="">PROYECTO NOMBRES JavaScript</a>
+			<a class="navbar-brand" href="">PROYECTO NOMBRES ALEATORIOS CON JavaScript</a>
 		</div>
 	</nav>
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
-		<h3 class="text-warning"> Generador de Nombres</h3>
+		<h3 class="text-primary"> Generador de Nombres Y Apellidos</h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<div class="col-md-3"></div>
 		<div class="col-md-6">
-			<button class="btn btn-danger btn btn-block" onclick="generate()">Generar Nombre</button>
+			<button class="btn btn-danger btn btn-block" onclick="generate()">Generar Nombres y a pellidos</button>
 			<div id="result"></div>
 		</div>
 	</div>
+	<p>creado el 9 de septiembre 2023</p>
 <script src="js/script.js"></script>
 </body>
 </html>
